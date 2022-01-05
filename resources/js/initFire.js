ace4fc1c9c3d56b652f3efff4a9d89ab13e02f89
HemarDev <94705124+HemarDev@users.noreply.github.com>
2022-01-05 20:22:35 +0100

    Add files via upload

---
+++ resources/js/initFire.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+const firebaseConfig = {
+apiKey: "AIzaSyDsqhb9QNpUOjfgTP0TeOxVgxJeRiZhcAk",
+authDomain: "hemar-dev.firebaseapp.com",
+databaseURL: "https://hemar-dev-default-rtdb.firebaseio.com",
+projectId: "hemar-dev",
+storageBucket: "hemar-dev.appspot.com",
+messagingSenderId: "254448944188",
+appId: "1:254448944188:web:b7f17ae1d56140745d2f39"
+};
+// Initialize Firebase
+initializeApp(firebaseConfig);
